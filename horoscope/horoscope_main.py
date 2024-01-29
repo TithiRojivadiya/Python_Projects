c5609e87799c465c8dc10b738620b028b6ba46f5
TithiRojivadiya <trojivadiya@gmail.com>
2024-01-30 05:19:01 +0530

    making window in horoscope

--- horoscope/horoscope_main.py
+++ horoscope/horoscope_main.py
@@ -1,4 +1,15 @@
 import requests
-import tkinter
+from tkinter import *
+from tkinter import ttk
 import PIL
 import playsound
+
+window = Tk()
+window.title("Horoscope app by Tithi")
+window.geometry("500x700")
+
+window.mainloop()
+
+# API
+# "sunsign_list": "http://sandipbgt.com/theastrologer/api/sunsigns",
+# "today": "http://sandipbgt.com/theastrologer/api/horoscope/{sunsign}/today",
